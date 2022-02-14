phhusson/platform_system_vold

Log support for exfat/texfat FS driver names
Change-Id: I5ae38741374b25b84595a9eae9bdda7afe2cac05

main.cpp
@@ -69,7 +69,9 @@ int main(int argc, char** argv) {
     LOG(DEBUG) << "Detected support for:"
                << (android::vold::IsFilesystemSupported("ext4") ? " ext4" : "")
                << (android::vold::IsFilesystemSupported("f2fs") ? " f2fs" : "")
-               << (android::vold::IsFilesystemSupported("vfat") ? " vfat" : "");
+               << (android::vold::IsFilesystemSupported("vfat") ? " vfat" : "")
+               << (android::vold::IsFilesystemSupported("exfat") ? " exfat" : "")
+               << (android::vold::IsFilesystemSupported("texfat") ? " texfat" : "");
 
     VolumeManager* vm;
     NetlinkManager* nm;

protonaosp-gsi/android_system_vold

main.cpp
@@ -67,8 +67,11 @@ int main(int argc, char** argv) {
     ATRACE_BEGIN("main");
 
     LOG(DEBUG) << "Detected support for:"
+               << (android::vold::IsFilesystemSupported("exfat") ? " exfat" : "")
+               << (android::vold::IsFilesystemSupported("texfat") ? " texfat" : "")
                << (android::vold::IsFilesystemSupported("ext4") ? " ext4" : "")
                << (android::vold::IsFilesystemSupported("f2fs") ? " f2fs" : "")
+               << (android::vold::IsFilesystemSupported("ntfs") ? " ntfs" : "")
                << (android::vold::IsFilesystemSupported("vfat") ? " vfat" : "");
 
     VolumeManager* vm;